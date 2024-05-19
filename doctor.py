62e97e4d804207fbedd449aa928eec60764fc98d
Prabhashi Karunathilake <prabhashikarunathilake@gmail.com>
2024-05-19 23:42:49 +0530

    Modify doctor.py to add appointment schedule feature

--- doctor.py
+++ doctor.py
@@ -3,6 +3,10 @@ class Doctor:
         self.name = name
         self.specialty = specialty
         self.doctor_id = doctor_id
+        self.appointment_schedule = []
+
+    def add_appointment(self, appointment):
+        self.appointment_schedule.append(appointment)
 
     def __str__(self):
         return f"Doctor ID: {self.doctor_id}, Name: {self.name}, Specialty: {self.specialty}"
--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from patient import Patient
 from doctor import Doctor
 def main():
+    # create a patient
     patient1 = Patient("Gayani", 60, "P001")
+
+    # create a doctor
     doctor1 = Doctor("Dr. Julian", "Dermatology", "D001")
+    doctor1.add_appointment("2024-06-01, 10:00 AM")
 
     print(patient1)
     print(doctor1)

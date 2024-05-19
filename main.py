e6eb90ec1f2a8a060e467ef381a7dcedc1705b16
Prabhashi Karunathilake <prabhashikarunathilake@gmail.com>
2024-05-19 23:24:20 +0530

    Modified patient.py by adding medical history feature

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from patient import Patient
 def main():
+    #create a patient
     patient1 = Patient("Gayani", 60, "P001")
+    patient1.add_medical_record("2024-05-11: Consultation")
+    patient1.add_medical_record("2024-04-20: Checkup")
 
     print(patient1)
 
--- patient.py
+++ patient.py
@@ -3,6 +3,10 @@ class Patient:
         self.name = name
         self.age = age
         self.patient_id = patient_id
+        self.medical_history = []
+
+    def add_medical_record(self, record):
+        self.medical_history.append(record)
 
     def __str__(self):
         return f"Patient ID: {self.patient_id}, Name: {self.name}, Age: {self.age}"
